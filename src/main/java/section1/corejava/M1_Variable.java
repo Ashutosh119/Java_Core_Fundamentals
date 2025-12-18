dee2b09a0ab93efa3cb4c410b694c96759f0a134
Ashutosh119 <ashutoshmca119@gmail.com>
2025-12-18 21:07:06 +0530

    new file-> variable implements

---
+++ src/main/java/section1/corejava/M1_Variable.java
@@ -0,0 +1,11 @@
+package section1.corejava;
+
+public class M1_Variable {
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1 + num2 ;
+        System.out.println("The Result of given nums :" + result);
+    }
+}
