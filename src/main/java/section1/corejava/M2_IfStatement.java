ec48d76060e15de0b3eabec9bc7ede6d59f55db3
Ashutosh119 <ashutoshmca119@gmail.com>
2025-12-19 05:50:43 +0530

    new file-> IF statement impl

---
+++ src/main/java/section1/corejava/M2_IfStatement.java
@@ -0,0 +1,12 @@
+package section1.corejava;
+
+public class M2_IfStatement {
+    public static void main(String[] args) {
+
+        int x = 19;
+        if (x > 20)
+            System.out.println("Statement 1");
+        else
+            System.out.println("Statement 2");
+    }
+}
