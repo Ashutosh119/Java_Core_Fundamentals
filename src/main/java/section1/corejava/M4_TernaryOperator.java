b8e7248234d50d977d6282ac39650d935ed5e43d
Ashutosh119 <ashutoshmca119@gmail.com>
2025-12-20 20:59:52 +0530

    Ternary Oper file added and impl

---
+++ src/main/java/section1/corejava/M4_TernaryOperator.java
@@ -0,0 +1,14 @@
+package section1.corejava;
+
+public class M4_TernaryOperator {
+    public static void main(String[] args) {
+
+        int n =9;
+        int result =0;
+
+        if(n %2==0)
+            result =10;
+        else result =20;
+        System.out.println(result);
+    }
+}
