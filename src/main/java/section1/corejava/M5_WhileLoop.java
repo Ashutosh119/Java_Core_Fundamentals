3475559537d961e7eb823ed2a217ff7e5fe683f7
Ashutosh119 <ashutoshmca119@gmail.com>
2026-01-07 13:37:01 +0530

    New class While loop implement

---
+++ src/main/java/section1/corejava/M5_WhileLoop.java
@@ -0,0 +1,12 @@
+package section1.corejava;
+
+public class M5_WhileLoop {
+    public static void main(String[] args) {
+
+        int a = 2;
+        while (true)
+        {
+            System.out.println(a + "Hi");
+        }
+    }
+}
