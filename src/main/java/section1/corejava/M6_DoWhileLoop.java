9710cc4cdeb1fe10f2315827db778928474384dd
Ashutosh119 <ashutoshmca119@gmail.com>
2026-01-08 11:55:09 +0530

    do while condition implement

---
+++ src/main/java/section1/corejava/M6_DoWhileLoop.java
@@ -0,0 +1,13 @@
+package section1.corejava;
+
+public class M6_DoWhileLoop {
+    public static void main(String[] args) {
+
+        int i = 5;
+        do {                      //do while loop check once time of the condition print at least one.
+
+            System.out.println("Hi " + i);
+            i++;
+        }while (i<=4);
+    }
+}
