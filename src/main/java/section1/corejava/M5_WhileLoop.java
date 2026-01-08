8a1bcf4f131c186d75feb5542627494912b3d450
Ashutosh119 <ashutoshmca119@gmail.com>
2026-01-08 11:39:48 +0530

    line 11 nested loop impl

--- src/main/java/section1/corejava/M5_WhileLoop.java
+++ src/main/java/section1/corejava/M5_WhileLoop.java
@@ -3,10 +3,18 @@ package section1.corejava;
 public class M5_WhileLoop {
     public static void main(String[] args) {
 
-        int a = 2;
-        while (true)
+        int a = 1;
+        while (a<=4)           //while loop
         {
-            System.out.println(a + "Hi");
+            System.out.println("Hi " + a);
+            int j=1;
+            while (j<=3){                         //nested loop
+                System.out.println("hello");
+                j++;
+            }
+            a++;
+
         }
+
     }
 }
