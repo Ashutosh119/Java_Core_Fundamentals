05ef7414c8ba3c89788fe555f9cb2e8b0cd6dd62
Ashutosh119 <ashutoshmca119@gmail.com>
2026-01-08 15:22:04 +0530

    loops implement

---
+++ src/main/java/section1/corejava/TestCode.java
@@ -0,0 +1,24 @@
+package section1.corejava;
+
+public class TestCode {
+    public static void main(String[] args) {
+
+
+        for (int i=1; i<=5; i++){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        int j=1;
+        while (j <= 5 ){
+
+                System.out.print( j + " ");
+                j++;
+        }
+        System.out.println();
+        int k =1;
+        do {
+            System.out.print(k + " ");
+            k++;
+        } while (k <= 5);
+    }
+}
