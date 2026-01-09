c61092e910c587343cd959738723ebe010b10923
Ashutosh119 <ashutoshmca119@gmail.com>
2026-01-09 15:08:35 +0530

    static keyword impl in code

---
+++ src/main/java/section1/corejava/M7_StaticVariabal.java
@@ -0,0 +1,32 @@
+package section1.corejava;
+
+
+class Mobile{
+    String brand;
+    int price;
+    static  String name;    // Here i have declare name as static so its shared details with all name based obj
+
+    public void show(){
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+}
+
+public class M7_StaticVariabal {
+    public static void main(String[] args) {
+
+        Mobile obj1 = new Mobile();
+        obj1.brand ="Apple";
+        obj1.price =1500;
+        Mobile.name= "Smartphone";
+
+        Mobile obj2 = new Mobile();
+        obj2.brand ="Samsung";
+        obj2.price =2500;
+        Mobile.name= "Smartphone";
+
+        obj1.name= "phone"; // here this will shared details
+
+        obj1.show();
+        obj2.show();
+    }
+}
--- src/main/java/section1/corejava/TestCode.java
+++ src/main/java/section1/corejava/TestCode.java
@@ -4,21 +4,26 @@ public class TestCode {
     public static void main(String[] args) {
 
 
-        for (int i=1; i<=5; i++){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        int j=1;
-        while (j <= 5 ){
 
-                System.out.print( j + " ");
-                j++;
+                // TODO: Print numbers 1 to 5 using for loop
+
+                for (int i=1; i<=5; i++){
+                    System.out.print( i + " ");
+                }
+                System.out.println();
+                int j=1;
+                while (j <= 5 ){
+
+                    System.out.print( j + " ");
+                    j++;
+                }
+                System.out.println();
+                int k =1;
+                do {
+                    System.out.print(k + " ");
+                    k++;
+                } while (k <= 5);
+            }
         }
-        System.out.println();
-        int k =1;
-        do {
-            System.out.print(k + " ");
-            k++;
-        } while (k <= 5);
-    }
-}
+
+
