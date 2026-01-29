73536ee12ba6177e631d83463386f30e5d6bd647
Ashutosh119 <ashutoshmca119@gmail.com>
2026-01-29 12:28:57 +0530

    Initial commit

--- src/main/java/section1/corejava/Hello.java
+++ src/main/java/section1/corejava/Hello.java
@@ -1,5 +1,11 @@
 package section1.corejava;
 
+//public class SayHello{
+//    public static void main(String[] args) {
+//        System.out.println("Say hello public");
+//    }
+//}    Two public class not possible in a main method java program
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Java Ai");
